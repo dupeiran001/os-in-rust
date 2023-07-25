6d9cfe6bd57f31ecc6ec5e7f61ae671032f01c9f
杜沛然 <2083856903@qq.com>
2023-07-25 16:19:39 +0800

    CPU exception finsihed

---
+++ VGA_text_mode/src/main.rs
@@ -0,0 +1,20 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    panic!("Some Message");
+    loop {}
+}
